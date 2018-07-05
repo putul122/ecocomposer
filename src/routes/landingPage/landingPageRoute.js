021017b2c66c2be70e407bc815b6b18a8a73b55a
Shubhangi <shubhangi.thakur@neosofttech.com>
2018-07-05 19:26:22 +0530

    changes

--- src/routes/landingPage/landingPageRoute.js
+++ src/routes/landingPage/landingPageRoute.js
@@ -1,9 +1,9 @@
 import React from 'react'
-// import Register from '../../components/register/registerComponent'
+import Register from '../../components/register/registerComponent'
 import ProductDescription from '../../components/productDescription/productDescriptionComponent'
 import Header from '../../components/header/headerComponent'
 import FooterComponent from '../../components/footer/footerComponent'
-import LeftNavigation from '../../components/leftNavigation/leftNavComponent'
+// import LeftNavigation from '../../components/leftNavigation/leftNavComponent'
 
 class LandingPageRoute extends React.Component {
 	render () {
@@ -11,13 +11,13 @@ class LandingPageRoute extends React.Component {
   <div>
     <Header />
     <div className='m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body'>
-      <LeftNavigation />
+      {/* <LeftNavigation /> */}
       <div className='m-grid__item m-grid__item--fluid m-wrapper'>
         <div className='m-content  m-demo m-demo__preview'>
           <h1 className='display-2'>Composer Bla Bla ...............</h1>
           <div className='row'>
             <ProductDescription />
-            {/* <Register /> */}
+            <Register />
           </div>
         </div>
       </div>
