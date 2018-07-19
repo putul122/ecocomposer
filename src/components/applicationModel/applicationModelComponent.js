942c727937f4e5cb2d7f296e29a3c4ec4927f376
putul sahani <putul.sahani@neosofttech.com>
2018-07-19 08:32:45 +0530

    overlap issue

--- src/components/applicationModel/applicationModelComponent.js
+++ src/components/applicationModel/applicationModelComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import joint from 'jointjs/index'
 import $ from 'jquery/dist/jquery'
-// import _ from 'lodash'
+import _ from 'lodash'
 import styles from './applicationModelComponent.scss'
 
     var Shape = joint.dia.Element.define('graph.Shape', {
@@ -53,10 +53,10 @@ import styles from './applicationModelComponent.scss'
     var Link = joint.dia.Link.define('graph.Link', {
     attrs: {
         '.connection': {
-            stroke: 'black',
+            stroke: 'gray',
             strokeWidth: 1,
             cursor: 'pointer',
-            // pointerEvents: 'none',
+            pointerEvents: 'none',
             targetMarker: {
                 type: 'path',
                 fill: 'gray',
@@ -162,6 +162,8 @@ import styles from './applicationModelComponent.scss'
             graph.resetCells(cells)
         }
 
+        bindInteractionEvents(adjustVertices, graph, paper)
+
         paper.fitToContent({
             padding: this.options.padding,
             gridWidth: 10,
@@ -179,9 +181,9 @@ import styles from './applicationModelComponent.scss'
             ranker: 'network-simplex',
             rankDir: 'LR',
             align: '',
-            rankSep: 100,
-            edgeSep: 70,
-            nodeSep: 40
+            rankSep: 80,
+            edgeSep: 60,
+            nodeSep: 80
         }
     },
 
@@ -269,6 +271,92 @@ import styles from './applicationModelComponent.scss'
         template: ''// document.getElementById('link-controls-template').content
     })
 
+    // displaying multiple links between two elements in different paths
+    function adjustVertices (graph, cell) {
+        console.log('adjustVertices ,,,,,,,,,,,,')
+        // If the cell is a view, find its model.
+        cell = cell.model || cell
+        if (cell instanceof joint.dia.Element) {
+            _.chain(graph.getConnectedLinks(cell)).groupBy(function (link) {
+                // the key of the group is the model id of the link's source or target, but not our cell id.
+                return _.omit([link.get('source').id, link.get('target').id], cell.id)[0]
+            }).each(function (group, key) {
+                // If the member of the group has both source and target model adjust vertices.
+                if (key !== 'undefined') adjustVertices(graph, _.first(group))
+            })
+            return
+        }
+
+        // The cell is a link. Let's find its source and target models.
+        var srcId = cell.get('source').id || cell.previous('source').id
+        var trgId = cell.get('target').id || cell.previous('target').id
+
+        // If one of the ends is not a model, the link has no siblings.
+        if (!srcId || !trgId) return
+
+        var siblings = _.filter(graph.getLinks(), function (sibling) {
+            var _srcId = sibling.get('source').id
+            var _trgId = sibling.get('target').id
+
+            return (_srcId === srcId && _trgId === trgId) || (_srcId === trgId && _trgId === srcId)
+        })
+
+        switch (siblings.length) {
+        case 0:
+            // The link was removed and had no siblings.
+            break
+
+        case 1:
+            // There is only one link between the source and target. No vertices needed.
+            cell.unset('vertices')
+            break
+
+        default:
+
+            // There is more than one siblings. We need to create vertices.
+            // First of all we'll find the middle point of the link.
+            var srcCenter = graph.getCell(srcId).getBBox().center()
+            var trgCenter = graph.getCell(trgId).getBBox().center()
+            var midPoint = joint.g.line(srcCenter, trgCenter).midpoint()
+
+            // Then find the angle it forms.
+            var theta = srcCenter.theta(trgCenter)
+
+            // This is the maximum distance between links
+            var gap = 20
+
+            _.each(siblings, function (sibling, index) {
+                // We want the offset values to be calculated as follows 0, 20, 20, 40, 40, 60, 60 ..
+                var offset = gap * Math.ceil(index / 2)
+                // Now we need the vertices to be placed at points which are 'offset' pixels distant
+                // from the first link and forms a perpendicular angle to it. And as index goes up
+                // alternate left and right.
+                //
+                //  ^  odd indexes
+                //  |
+                //  |---->  index 0 line (straight line between a source center and a target center.
+                //  |
+                //  v  even indexes
+                var sign = index % 2 ? 1 : -1
+                var angle = joint.g.toRad(theta + sign * 90)
+                // We found the vertex.
+                var vertex = joint.g.point.fromPolar(offset, angle, midPoint)
+                sibling.set('vertices', [{ x: vertex.x, y: vertex.y }])
+            })
+        }
+    }
+
+    function bindInteractionEvents (adjustVertices, graph, paper) {
+        // bind `graph` to the `adjustVertices` function
+        var adjustGraphVertices = _.partial(adjustVertices, graph)
+
+        // adjust vertices when a cell is removed or its source/target was changed
+        graph.on('add remove change:source change:target', adjustGraphVertices)
+
+        // adjust vertices when the user stops interacting with an element
+        paper.on('cell:pointerup', adjustGraphVertices)
+    }
+
 class ApplicationModelComponent extends React.Component {
     construct (props) {}
     componentWillMount () {}
@@ -320,15 +408,16 @@ class ApplicationModelComponent extends React.Component {
                             height: 300,
                             width: 500,
                             interactive: function (cellView) {
+                                return cellView.model.isLink()
                                 // linkMove: false
-                                // return cellView.model.isElement();
+                                // return cellView.model.isElement()
                             }
                         }),
                         nodes: nodeArray,
                         connections: linkArray,
                         centerNode: nodeArray.length
                     }).on({
-                        'layout': LinkControls.refresh
+                        // 'layout': LinkControls.refresh
                     }, LinkControls)
                     controls.layout()
                 }
